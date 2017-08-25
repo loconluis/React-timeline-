34ed7add347abee41f3dfbe9a1ae85109cca2359
luislocon <luis.locon@gmail.com>
2017-08-24 22:18:32 -0600

    adding middlewares

--- source/store.js
+++ source/store.js
@@ -1,7 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 
 import reducer from './reducer';
 
-const store = createStore(reducer);
+// const logger = store => next => (action) => {
+//   console.debug('estado actual', store.getState());
+//   console.log('acción', action);
+//   const result = next(action);
+//   console.debug('estado nuevo', store.getState());
+//   return result;
+// };
+
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    createLogger(),
+  ),
+);
 
 export default store;
